bf064c53835582a385fa3ee64e6b48af7599f8b8
shangeetha283 <thevanesanshangeetha@gmail.com>
2024-12-24 02:27:05 +0530

    Update hello.py
    hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("hello,world!")
+import requests
+url = "https://raw.githubusercontent.com/shangeetha283/OpenUniGit003/main/hello.py"
+response = request.get(url)
+
+if response.status_code ==200:
+  print("File Content :")
+  print(response.text)
+else:
+  print(f"Failed to fetch file. HTTP Status Code:{response.status_code}")
